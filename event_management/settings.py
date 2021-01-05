2341adfaed6ac7a59be0ad177f9678a7246b1d5a
Shi Han NG <shihanng@gmail.com>
2021-01-05 21:36:47 +0900

    Add rest framework

--- event_management/settings.py
+++ event_management/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "event_management.events",
 ]
 
@@ -121,3 +122,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "events.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
