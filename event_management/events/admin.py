240e026522133b26ffb6a3f27876bda4368afd86
Shi Han NG <shihanng@gmail.com>
2021-01-06 19:42:49 +0900

    Register UserAdmin

--- event_management/events/admin.py
+++ event_management/events/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.models import Group
 
 from event_management.events.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(AuthUserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "is_staff",
+    )
+    list_filter = list_display
+    readonly_fields = ["last_login", "email"]
+    fieldsets = ((None, {"fields": ("email", "password", "is_staff")}),)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff"),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    filter_horizontal = ()
+    ordering = ("email",)
+
+
+admin.site.unregister(Group)
